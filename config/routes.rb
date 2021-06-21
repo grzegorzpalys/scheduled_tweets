245fabd4131b2dabf83bcc28f46c1d7d06b5d0a6
grzegorzpalys <grzegorzpalys@yahoo.co.uk>
2021-06-21 20:02:53 +0100

    Signing up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new" 
+
   root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
