854537352885bce26afb659db28e459d7f142172
grzegorzpalys <grzegorzpalys@yahoo.co.uk>
2021-08-24 18:11:23 +0100

    All

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,22 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Your Account was successfully created!"
+    else
+      flash[:alert] = "Something went wrong:("
+      render :new
+    end 
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :passwor_confirmation)
+  end
+
 end
